564fc90671539170df20bb4d283b3eb29357a3b9
Anatoly Mironov <mirontoli@gmail.com>
2016-10-23 14:48:25 +0200

    test react rendering 5

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var WordPresentation = React.createClass({
 	render: function() {
-		return <div>hej</div>;
+		return <div>{this.props.word.sv} : {this.props.word.cv}</div>;
 	}
 });
 var WordList = React.createClass({
 	render: function() {
 		var rows = [];
 	    this.props.words.forEach(function(word) {
-        	rows.push(<WordPresentation sv={word.sv} cv={word.cv} />);
+        	rows.push(<WordPresentation word={word} />);
 	    });
 		return <div>{rows}</div>;
 	}
