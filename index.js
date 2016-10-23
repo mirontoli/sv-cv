1bdcac9772185c9fae192e201b23dda01f16121a
Anatoly Mironov <mirontoli@gmail.com>
2016-10-23 14:42:44 +0200

    test react rendering 3

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ var WordPresentation = React.createClass({
 		return <div>hej</div>;
 	}
 });
+var WordList = React.createClass({
+	render: function() {
+		var rows = [];
+	    this.props.words.forEach(function(word) {
+        	rows.push(<WordPresentation sv={word.sv} cv={word.cv} />);
+	    });
+		return <div>{rows}</div>;
+	}
+});
 
-ReactDOM.render(<WordPresentation />, document.getElementById('root'));
+jQuery.getJSON("dict.json").done(function(data){
+	var WORDS = data.dictionary.words;
+	ReactDOM.render(<WordList words={WORDS}/>, document.getElementById('root'));
+});
+
 
