3f215660aab64db9fdd52bc793810d62d366b4ea
Anatoly Mironov <mirontoli@gmail.com>
2016-11-07 07:11:15 +0100

    update dictionary

--- import.js
+++ import.js
@@ -2,7 +2,7 @@
 var endpoint = "https://takana14.sharepoint.com/sites/pa/_api/web/lists/getbytitle('Swedish')/Items?$select=Title,cv,Body&$orderby=Title"
 
 //jquery load
-var jq = document.createElement('script');jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(jq); setTimeout(function() {jQuery.noConflict(); console.log('jQuery loaded'); }, 1000);void(0);
+var jq = document.createElement('script');jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(jq); setTimeout(function() {jQuery.noConflict(); console.log('jQuery loaded'); extract(); }, 1000);void(0);
 
 function extract() {
   jQuery.getJSON(endpoint)
@@ -23,4 +23,3 @@ function extract() {
   });
 }
 
-extract()
