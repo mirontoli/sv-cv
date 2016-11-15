897a9de8fbe61c3835184ae53aa5dd3a860dcb2c
Anatoly Mironov <mirontoli@gmail.com>
2016-11-15 15:17:02 +0100

    add new words

--- import.js
+++ import.js
@@ -1,5 +1,5 @@
 // this is code for importing data from the SharePoint Online list
-var endpoint = "https://takana14.sharepoint.com/sites/pa/_api/web/lists/getbytitle('Swedish')/Items?$select=Title,cv,Body&$orderby=Title"
+var endpoint = "https://takana14.sharepoint.com/sites/pa/_api/web/lists/getbytitle('Swedish')/Items?$select=Title,cv,Body&$orderby=Title&$top=1000000"
 
 //jquery load
 var jq = document.createElement('script');jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(jq); setTimeout(function() {jQuery.noConflict(); console.log('jQuery loaded'); extract(); }, 1000);void(0);
